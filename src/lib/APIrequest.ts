f44008e800dbf9ed281f8ae195ae86505ae9f9ef
KaninchenSpeed <fhs222@outlook.de>
2021-11-20 18:30:26 +0100

    added post request support

--- src/lib/APIrequest.ts
+++ src/lib/APIrequest.ts
@@ -3,6 +3,8 @@ import { API_ROOT, VERSION_PREFIX } from './api'
 
 export interface RequestOptions {
     api_key?: string
+    query?: { [key: string]: any }
+    body?: { [key: string]: any }
 }
 
 export interface Pagination {
@@ -21,13 +23,27 @@ export interface Response<DataType> {
  * Requests content from the CurseForge API
  */
 export default function APIRequest<DataType>(path: string, options?: RequestOptions) {
-    return axios.get<Response<DataType>>(`${API_ROOT}/${VERSION_PREFIX}${path}`, {
-        headers: {
-            'Accept': 'appliction/json',
-            'x-api-key': options?.api_key ?? ''
-        },
-        transitional: {
-            silentJSONParsing: true
-        }
-    })
+    const params = Object.keys(options?.query ?? {}).map(key => {return { name: key, value: String(options?.query ? options?.query[key] : undefined) }})
+    if (options?.body) {
+        return axios.post<Response<DataType>>(`${API_ROOT}/${VERSION_PREFIX}${path}${params.length > 0 ? `?${params.map(p => `${p.value}=${p.value}`).join('&')}` : ''}`, options.body, {
+            headers: {
+                'Accept': 'appliction/json',
+                'Content-Type': 'appliction/json',
+                'x-api-key': options?.api_key ?? ''
+            },
+            transitional: {
+                silentJSONParsing: true
+            }
+        })
+    } else {
+        return axios.get<Response<DataType>>(`${API_ROOT}/${VERSION_PREFIX}${path}${params.length > 0 ? `?${params.map(p => `${p.value}=${p.value}`).join('&')}` : ''}`, {
+            headers: {
+                'Accept': 'appliction/json',
+                'x-api-key': options?.api_key ?? ''
+            },
+            transitional: {
+                silentJSONParsing: true
+            }
+        })
+    }
 }
